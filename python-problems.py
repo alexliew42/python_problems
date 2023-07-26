7520e03c65869bf9020b5c24beb5e746ef700d78
Alex Liew <liewtwoo42@gmail.com>
2023-07-26 15:23:36 -0400

    Jumping on Clouds Hackerrank

--- python-problems.py
+++ python-problems.py
@@ -13,21 +13,43 @@
 
 # findDigits(n)
 
-k = 2
-c = [0, 0, 1, 0, 0, 1, 1, 0]
+# k = 2
+# c = [0, 0, 1, 0, 0, 1, 1, 0]
 
-def jumpingOnClouds(c, k):
-  energy = 100
+# def jumpingOnClouds(c, k):
+#   energy = 100
+#   i = 0
+#   while True:
+#     i += k
+#     energy -= 1
+#     if i >= len(c):
+#       i = i % len(c)
+#     if c[i] == 1:
+#       energy -= 2
+#     if i == 0:
+#       break
+#   return(energy)
+
+# jumpingOnClouds(c, k)
+
+c = [0, 0, 0, 1, 0, 0]
+def jumpingOnClouds(c):
   i = 0
-  while True:
-    i += k
-    energy -= 1
-    if i >= len(c):
-      i = i % len(c)
-    if c[i] == 1:
-      energy -= 2
-    if i == 0:
-      break
-  return(energy)
+  total = 0
+  while i < len(c) - 1 :
+    print(i)
+    if i == len(c) - 2:
+      total += 1
+      i += 1
+    elif c[i + 2] == 0:
+      total += 1
+      i += 2
+    else:
+      i += 1
+      total += 1
+    
+  print(total)
+  
 
-jumpingOnClouds(c, k)
+jumpingOnClouds(c)
+    
